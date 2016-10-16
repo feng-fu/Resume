830cf477bbbee8e108ea2c588463dc15afc28616
code-baby <zhucefengfu@foxmail.com>
2016-10-16 23:10:13 +0800

    Resume

--- js/top.js
+++ js/top.js
@@ -1,24 +1,29 @@
 function Top($node){
   this.node = $node;
   this.node.data("show",false);
+  this.scroll();
   this.bind();
 }
 Top.prototype.bind = function(){
   var self = this;
   var $document = $(document);
   $document.on("scroll",function(){
-    if(!self.node.data("show") && $document.scrollTop() > 200){
-
-      self.node.show();
-      self.node.data("show",true);
-    }
-    if(self.node.data("show") && $document.scrollTop() < 200){
-      self.node.hide();
-      self.node.data("show",false);
-    }
+    self.scroll();
   });
   this.node.on("click",function(){
     $("html,body").animate({scrollTop:0},$(document).scrollTop()/2)
   })
+}
+Top.prototype.scroll = function(){
+  var $document = $(document);
+  if(!this.node.data("show") && $document.scrollTop() > 200){
+
+    this.node.show();
+    this.node.data("show",true);
+  }
+  if(this.node.data("show") && $document.scrollTop() < 200){
+    this.node.hide();
+    this.node.data("show",false);
+  }
 }
 new Top($("#top"))
